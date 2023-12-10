a6e1dc4b12d65f27ad1f4cecdd4cf063312f0081
Wily Corzo <ing.willy.corzo@gmail.com>
2023-12-10 16:56:17 -0500

    Inicio Fullstack

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+const Display = ({ counter }) => {
+  return <div>{counter}</div>
+}
+
+const Button = ({ onSmash, text }) => {
+  return <button onClick={onSmash}>{text}</button>
+}
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <>
+      <div>
+        <p>
+          Hello {name}, you are {age} years old
+        </p>
+        <p>So you were probably born in {bornYear()}</p>
+      </div>
+    </>
+  )
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
+  return (
+    <>
+      <div>
+        <Display counter={counter} />
+      </div>
+      <Button handleClick={decreaseByOne} text='Minus' />
+      <Button handleClick={setToZero} text='Zero' />
+      <Button handleClick={increaseByOne} text='Plus' />
+
+      <div>
+        <h1>Gretings</h1>Gretings
+        <Hello name='George' age={100} />
+        <Hello name='Maya' age={26 + 10} />
+        <Hello name='Willy' age={40} />
+      </div>
+    </>
+  )
+}
+
+export default App
