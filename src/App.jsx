3f56d0cff54d0339b6559df4d395a388eb23bce8
Wily Corzo <ing.willy.corzo@gmail.com>
2023-12-16 08:29:58 -0500

    Terminado parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,21 @@ const Hello = ({ name, age }) => {
   )
 }
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons </div>
+  }
+
+  return <div>button press history: {props.allClicks.join(' ')}</div>
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
   console.log('rendering with counter value', counter)
 
   const increaseByOne = () => {
@@ -41,8 +54,36 @@ const App = () => {
     setCounter(0)
   }
 
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    console.log('left before', left)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    console.log('left after', updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    console.log('right before', right)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    console.log('right after', updatedRight)
+    setTotal(left + updatedRight)
+  }
+
   return (
     <>
+      <div>
+        {left}
+        <Button onSmash={handleLeftClick} text='Left'/>
+        <Button onSmash={handleRightClick} text='Right'/>
+        
+        {right}
+        <History allClicks={allClicks} />
+        <p>total: {total}</p>
+      </div>
+
       <div>
         <Display counter={counter} />
       </div>
