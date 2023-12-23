b11570508621014a67ef217491d122ce67816845
Wily Corzo <ing.willy.corzo@gmail.com>
2023-12-23 07:37:18 -0500

    Notes 23

--- src/App.jsx
+++ src/App.jsx
@@ -2,87 +2,56 @@ import { useState } from 'react'
 
 import Note from './components/Note'
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>
-}
-
 const Button = ({ onSmash, text }) => {
   return <button onClick={onSmash}>{text}</button>
 }
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
 
-  return (
-    <>
-      <div>
-        <p>
-          Hello {name}, you are {age} years old
-        </p>
-        <p>So you were probably born in {bornYear()}</p>
-      </div>
-    </>
-  )
-}
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new Note')
+  const [showAll, setShowAll] = useState(true)
 
-const App = ({ notes }) => {
-  const [counter, setCounter] = useState(0)
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1
+    }
 
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  const result = notes.map((note) => note.id)
-  console.log(result)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <>
       <div>
         <h1>Notes</h1>
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'Important' : 'All'}
+          </button>
+        </div>
         <ul>
-          <li>{notes[0].content}</li>
-          <li>{notes[1].content}</li>
-          <li>{notes[2].content}</li>
-        </ul>
-      </div>
-
-      <div>
-        <h1>Notes</h1>
-        <ul>
-          {notes.map((note) => (
+          {notesToShow.map((note) => (
             <Note key={note.id} note={note} />
           ))}
         </ul>
       </div>
-      <div>
-        <Display counter={counter} />
-      </div>
-      <Button handleClick={decreaseByOne} text='Minus' />
-      <Button handleClick={setToZero} text='Zero' />
-      <Button handleClick={increaseByOne} text='Plus' />
 
-      <div>
-        <h1>Gretings</h1>Gretings
-        <Hello name='George' age={100} />
-        <Hello name='Maya' age={26 + 10} />
-        <Hello name='Willy' age={40} />
-      </div>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type='submit'>save</button>
+      </form>
     </>
   )
 }
